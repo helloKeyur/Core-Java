3e189e0fef2eacf7267c75cb3cff8f2f53d1a923
Keyur Vamja <keyurvamja@Keyurs-MacBook-Air-M1.local>
2023-03-16 23:43:53 +0530

    EP1: Resereved Words in JAVA.

---
+++ EP1-IDENTIFIERS/Ep1ReservedWords.java
@@ -0,0 +1,103 @@
+/*
+	@author KEYUR-VAMJA 
+	@since 16-03-2023
+	@ref DURGASOFT CORE-JAVA YT
+*/
+class Ep1ReservedWords{
+		
+	public static void main(String args[]){
+		/*
+		
+		#RESERVED WORDS
+		-> In Java, Some words are reserved to represent some meaningfull functionality, such type of words are called Reserved words.
+		-> In Java, return type is mandatory. if method don't return anything then we have to declare that method as void return type. but in c lang return type is optional, and default return type is int;
+		-> all 53 Resereved word in java contains only lowercase alphabet symbols.
+		-> In Java, we have only `new` keyword & there is no `delete` keywords. because destraction of useless objects is responsibility of garbeg collector.
+		
+		=> strictfp, enum, assert keywords are new in java.
+
+		=> There are 53 Reserved Words.
+			
+			A). Keywords (50)
+				i). Used Keywords (48)
+					if, else, switch, case, .....
+				ii). Unused Keywords (2)
+					01. goto - uses of goto created several problems in old langauges hence java sun people baned this keyword in java.
+					02. const - use final instead of const.
+					
+					both are unused keywords and if we are trying to use we will get compile time error.
+
+			B). Reserved Literals (3) - in the forms of values.
+				01. true - value for boolean datatype.
+				02. false - value for boolean datatype.
+				03. null - default values for Object reference
+		
+		#LIS OF USED KEYWORDS (48)
+			=> Keywords for DataTypes (8)
+				01. byte
+				02. short
+				03. int
+				04. long
+				05. float
+				06. double
+				07. boolean
+				08. char
+
+			=> Keywords for Flow Control (11)
+				01. if
+				02. else
+				03. while
+				04. do
+				05. break
+				06. for
+				07. switch
+				08. case
+				09. default
+				10. continue
+				11. return
+
+			=> Keywords for modifiers (11)
+				01. public
+				02. private
+				03. protected
+				04. static
+				05. final
+				06. synchronized
+				07. native
+				08. strictfp (@since 1.2 version)
+				09.	abstract
+				10. transient
+				11. volatile
+			
+			=> Keywords for Exception handling (6)
+				01. try
+				02. catch
+				03. finally
+				04. throw
+				05. throws
+				06. assert (@since 1.4 version)
+
+			=> Class realated keywords (6)
+				01. class
+				02. interface
+				03. extends
+				04. implements
+				05. package
+				06. import
+
+			=> Object releated keywords (4)
+				01. new
+				02. instanceof
+				03. super
+				04. this
+
+			=> void return type keyword (1)
+				01. void
+
+			=> enum keyword (1)
+				01. enum (@since 1.5 version) - We can use enum to define group of named constant.
+				
+		*/
+		System.out.println("Read the comment line code to know about the JAVA Reserved Words.");
+	}
+}
